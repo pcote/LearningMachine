523866e7ab48f23698f851d24923d450f9399ada
Phil Cote <cotejrp@gmail.com>
2016-03-03 12:27:15 -0500

    Refactor: Moved test user info to setup.

--- roles/main/files/view_tests.py
+++ roles/main/files/view_tests.py
@@ -6,7 +6,9 @@ from pdb import set_trace
 
 class ViewTestCase(unittest.TestCase):
     def setUp(self):
-        pass
+        self.test_user_id = "dummyuser@somewhere.com"
+        self.test_display_name = "Dummy User"
+
 
     def get_json(self, res):
         raw_data = res.data
@@ -22,29 +24,26 @@ class ViewTestCase(unittest.TestCase):
 
 
     def testUserInfo(self):
-        test_user_id = "dummyuser@somewhere.com"
-        test_display_name = "Dummy User"
 
         with app.test_client() as client:
             with client.session_transaction() as sess:
-                sess["email"] = test_user_id
-                sess["display_name"] = test_display_name
+                sess["email"] = self.test_user_id
+                sess["display_name"] = self.test_display_name
 
             res = client.get("/userinfo")
             json_data = self.get_json(res)
             print(json_data)
             self.assertIn("displayName", json_data)
             self.assertIn("email", json_data)
-            self.assertEqual(test_user_id, json_data["email"])
-            self.assertEqual(test_display_name, json_data["displayName"])
+            self.assertEqual(self.test_user_id, json_data["email"])
+            self.assertEqual(self.test_display_name, json_data["displayName"])
 
 
 
     def testExerciseHistory(self):
-        test_user_id = "dummyuser@email.com"
         with app.test_client() as client:
             with client.session_transaction() as sess:
-                sess["email"] = test_user_id
+                sess["email"] = self.test_user_id
 
             result = client.get("/exercisehistory")
             json_data = self.get_json(result)
