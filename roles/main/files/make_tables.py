38ce4262a8e158956a4c91be6e97585285b10466
Phil Cote <cotejrp@gmail.com>
2016-03-03 18:12:06 -0500

    Having a table creation script make tables before the learning machine script kicks into gear.  That way, the public db account that the service uses can get by without needing create privileges.

---
+++ roles/main/files/make_tables.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Ensures that the tables that need to exist do exist.
+"""
+from configparser import ConfigParser
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    cp = ConfigParser()
+    dir_path = __file__.rsplit("/", maxsplit=1)[0]
+    config_file_name = "{}/{}".format(dir_path, "config.ini")
+    cp.read(config_file_name)
+
+    db_section = cp["learningmachine"]
+    user, root_password = "root", db_section.get("root_password")
+    host, db = db_section.get("host"), db_section.get("db")
+    db_url = "mysql+pymysql://{}:{}@{}/{}".format(user, root_password, host, db)
+    eng = create_engine(db_url)
+    from model import meta
+    meta.create_all(bind=eng)
+    print("all tables set up")
