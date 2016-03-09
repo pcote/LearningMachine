1bdbcdbfb920e917f2c2c4f2c75775e8873c3477
Phil Cote <cotejrp@gmail.com>
2016-03-09 10:06:41 -0500

    Expanded unit testing to cover all flask view endpoints.

--- roles/main/files/view_tests.py
+++ roles/main/files/view_tests.py
@@ -16,6 +16,11 @@ class ViewTestCase(unittest.TestCase):
         json_ob = json.loads(string_data)
         return json_ob
 
+    def make_json_text(self, dct):
+        raw_data = json.dumps(dct)
+        raw_data = raw_data.encode("utf8")
+        return raw_data
+
 
     def testWelcomePage(self):
         with app.test_client() as client:
@@ -38,6 +43,74 @@ class ViewTestCase(unittest.TestCase):
             self.assertEqual(self.test_user_id, json_data["email"])
             self.assertEqual(self.test_display_name, json_data["displayName"])
 
+
+    def testAddScore(self):
+        import model
+        test_exercise_id = 1
+        test_score = 1
+        test_dict = dict(exercise_id=test_exercise_id, score=test_score)
+        mock = MagicMock(return_value=dict(result="success"))
+        model.add_attempt = mock
+
+        with app.test_client() as client:
+            with client.session_transaction() as sess:
+                sess["email"] = self.test_user_id
+                sess["display_name"] = self.test_display_name
+
+            headers = {"Content-type": "application/json"}
+            data = self.make_json_text(test_dict)
+            client.post("/addscore", headers=headers, data=data)
+            mock.assert_called_with(test_exercise_id, test_score)
+
+    def testAddExercise(self):
+        import model
+        test_question = "Test Question?"
+        test_answer = "Test Answer"
+        test_dict = dict(new_question=test_question, new_answer=test_answer)
+
+        mock = MagicMock()
+        model.add_exercise = mock
+
+        with app.test_client() as client:
+            with client.session_transaction() as sess:
+                sess["email"] = self.test_user_id
+
+            headers={"Content-type": "application/json"}
+            data = self.make_json_text(test_dict)
+            client.post("/addexercise", headers=headers, data=data)
+            mock.assert_called_with(test_question, test_answer, self.test_user_id)
+
+    def testDeleteExercise(self):
+        import model
+        test_exercise_id = 1
+        test_dict = dict(exercise_id=test_exercise_id)
+        mock = MagicMock()
+        model.delete_exercise = mock
+
+        with app.test_client() as client:
+            with client.session_transaction() as sess:
+                sess["email"] = self.test_user_id
+
+            headers = {"Content-type": "application/json"}
+            data = self.make_json_text(test_dict)
+            client.post("/deleteexercise", headers=headers, data=data)
+            mock.assert_called_with(self.test_user_id, test_exercise_id)
+
+
+    def testGetExercises(self):
+        import model
+        empty_list = []
+        mock = MagicMock(return_value=empty_list)
+        model.get_all_exercises = mock
+
+        with app.test_client() as client:
+            with client.session_transaction() as sess:
+                sess["email"] = self.test_user_id
+
+            result = client.get("/exercises")
+            json_data = self.get_json(result)
+            mock.assert_called_with(self.test_user_id)
+            self.assertTrue("exercises" in json_data)
 
 
     def testExerciseHistory(self):
