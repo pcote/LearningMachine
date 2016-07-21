9cd0f1ed44901b79b2f4634a4372cd8dcc50c21d
Phil Cote <cotejrp@gmail.com>
2016-07-21 14:52:03 -0400

    Bring in the main component folder that was forgotten a couple of commits back. (oops)

---
+++ roles/main/files/static/main_app/flashmark_main.js
@@ -0,0 +1,49 @@
+var MainController = function(userService, exerciseService){
+
+    var mc = this;
+    userService.showUserName(mc);
+
+    // Controls the visibility of the topics and exercise sections
+    mc.showStatus = {};
+    mc.showStatus.exercises = true;
+    mc.showStatus.attempts = false;
+    mc.showStatus.addFlashmarkButton = true;
+
+
+    // Exercise attempts report data.
+    mc.report = {};
+    mc.report.attempts = [];
+
+    mc.viewAttemptsClick = function(){
+        mc.showStatus.exercises = false;
+        mc.showStatus.attempts = true;
+        mc.showStatus.addFlashmarkButton = false;
+
+        var cbSuccess = function(res){
+            mc.report.attempts = res.data.history;
+        };
+
+        var cbFailure = function(res){
+            alert("attempt to get the attempt report failed");
+        }
+
+        exerciseService.getAttemptsReport().then(cbSuccess, cbFailure);
+    };
+
+    mc.viewExercisesClick = function(){
+        mc.showStatus.exercises = true;
+        mc.showStatus.attempts = false;
+        mc.showStatus.addFlashmarkButton = true;
+    };
+
+};
+
+var flashmarkApp = function(){
+    var d = {};
+    d.restrict = "A";
+    d.scope = {};
+    d.templateUrl = "flashmark_main.html";
+    d.controller = "MainController";
+    d.controllerAs = "mc";
+    return d;
+};
