2c52061042d6a6a727260861e2c4b8827bb38b70
Phil Cote <cotejrp@gmail.com>
2016-07-21 14:46:52 -0400

    Bring in the separate component folders.

---
+++ roles/main/files/static/attempts_report/attempts_report.js
@@ -0,0 +1,30 @@
+var lmScoreWordFilter = function(){
+    var filter = function(score){
+        if(score == 1 ){
+            return "Bad";
+        }
+        else if(score == 2){
+            return "Okay";
+        }
+        else if(score == 3){
+            return "Good";
+        }
+        else{
+            return "ERROR: Bad score argument";
+        }
+    };
+
+    return filter;
+};
+
+var attemptsReport = function(){
+    var d = {};
+    d.restrict = "E";
+    d.scope = {
+        attempts: "=",
+        show: "="
+    };
+
+    d.templateUrl = "attempts_report.html"
+    return d;
+};
