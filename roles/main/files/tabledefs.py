f11d9b3d695844a7ef90f8cbd533cec7b235b556
Phil Cote <cotejrp@gmail.com>
2016-12-23 10:42:31 -0500

    New table defs for exercise tags and the association table it connects to exercises with.

--- roles/main/files/tabledefs.py
+++ roles/main/files/tabledefs.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Text, Integer, ForeignKey, TIMESTAMP, VARCHAR, Table, MetaData
+from sqlalchemy import Column, Text, Integer, ForeignKey, TIMESTAMP, VARCHAR, Table, MetaData, ForeignKeyConstraint
 meta = MetaData()
+
 
 user_table = Table("users", meta,
                    Column("email", VARCHAR(255), primary_key=True),
@@ -33,3 +34,12 @@ resource_by_exercise_table = Table("resources_by_exercise", meta,
                            Column("exercise_id", Integer, ForeignKey("exercises.id"), primary_key=True))
 
 
+exercise_tag_table = Table("exercise_tags", meta,
+                           Column("name", VARCHAR(255), primary_key=True),
+                           Column("user_id", ForeignKey("users.email"), primary_key=True))
+
+exercise_by_exercise_tags_table = Table("exercises_by_exercise_tags", meta,
+                                        Column("exercise_id", ForeignKey("exercises.id"), primary_key=True),
+                                        Column("tag_name", VARCHAR(255), primary_key=True),
+                                        Column("user_id", VARCHAR(255), primary_key=True),
+                                        ForeignKeyConstraint(["tag_name", "user_id"], ["exercise_tags.name", "exercise_tags.user_id"]))
